201a4113611b2d5fd528fede8791782ab7ea83ba
Cristiano Morgante <cristianomorgante@hotmail.com>
2019-01-19 00:38:52 +0000

    Desafio aula [023]

--- src/objects/cards-wrapper/index.js
+++ src/objects/cards-wrapper/index.js
@@ -4,3 +4,11 @@ const $cardsWrapper = document.createElement("section");
 $cardsWrapper.classList.add("cards-wrapper");
 
 $root.insertBefore($cardsWrapper, null);
+
+$cardsWrapper.addEventListener("click", event => {
+
+    const $target = event.target;
+    if ($target.tagName === "ARTICLE" | $target.tagName === "IMG") {
+        console.log('foi');
+    }
+})
