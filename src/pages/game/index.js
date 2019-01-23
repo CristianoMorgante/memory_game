3feedc24844039c2a1357599e0935d4a2baaefbe
Cristiano Morgante <cristianomorgante@hotmail.com>
2019-01-23 18:01:48 +0000

    Desafio básico [025]

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,16 +1,20 @@
 const $root = document.querySelector("#root");
 const $cardsWrapper = createCardsWrapper();
-const $memoryCard = createMemoryCard("img/icon-collabcode.png", "Gueio mascote da CollabCode");
-const $memoryCardActive = createMemoryCard("img/icon-c.png", "Ícone de um livro da linguagem C++",
+const $CardGueio = createMemoryCard("img/icon-collabcode.png", "Gueio mascote da CollabCode");
+const $CardCsharp = createMemoryCard("img/icon-c.png", "Ícone de um livro da linguagem C++",
     "-active");
+const $CardPHP = createMemoryCard("img/icon-php.png", "Ícone de um livro de PHP", "-active");
+const $CardJS = createMemoryCard("img/icon-js.png", "Ícone de um livro de Javascript", "-active");
+const $CardCabeca = createMemoryCard("img/icon-cabeca.png", "Imagem de uma cabeça com tag dentro", "-active");
 
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardActive);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardActive);
+
+$cardsWrapper.insertAdjacentHTML("beforeend", $CardCabeca);
+$cardsWrapper.insertAdjacentHTML("beforeend", $CardCabeca);
+$cardsWrapper.insertAdjacentHTML("beforeend", $CardJS);
+$cardsWrapper.insertAdjacentHTML("beforeend", $CardJS);
+$cardsWrapper.insertAdjacentHTML("beforeend", $CardPHP);
+$cardsWrapper.insertAdjacentHTML("beforeend", $CardPHP);
+$cardsWrapper.insertAdjacentHTML("beforeend", $CardCsharp);
+$cardsWrapper.insertAdjacentHTML("beforeend", $CardCsharp);
 
 $root.insertAdjacentElement("beforeend", $cardsWrapper);
