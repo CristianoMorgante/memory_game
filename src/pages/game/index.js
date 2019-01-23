fad9694dc759e50c8baeb20506bb341c385011b0
Cristiano Morgante <cristianomorgante@hotmail.com>
2019-01-23 19:21:57 +0000

    Desafio 2 aula [025]

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,12 +1,29 @@
 const $root = document.querySelector("#root");
 const $cardsWrapper = createCardsWrapper();
-const $CardGueio = createMemoryCard("img/icon-collabcode.png", "Gueio mascote da CollabCode");
-const $CardCsharp = createMemoryCard("img/icon-c.png", "Ícone de um livro da linguagem C++",
-    "-active");
-const $CardPHP = createMemoryCard("img/icon-php.png", "Ícone de um livro de PHP", "-active");
-const $CardJS = createMemoryCard("img/icon-js.png", "Ícone de um livro de Javascript", "-active");
-const $CardCabeca = createMemoryCard("img/icon-cabeca.png", "Imagem de uma cabeça com tag dentro", "-active");
-
+const $CardGueio = createMemoryCard({
+  src: "img/icon-collabcode.png",
+  alt: "Gueio mascote da CollabCode"
+});
+const $CardCsharp = createMemoryCard({
+  src: "img/icon-c.png",
+  alt: "Ícone de um livro da linguagem C++",
+  nameClass: "-active"
+});
+const $CardPHP = createMemoryCard({
+  src: "img/icon-php.png",
+  alt: "Ícone de um livro de PHP",
+  nameClass: "-active"
+});
+const $CardJS = createMemoryCard({
+  src: "img/icon-js.png",
+  alt: "Ícone de um livro de Javascript",
+  nameClass: "-active"
+});
+const $CardCabeca = createMemoryCard({
+  src: "img/icon-cabeca.png",
+  alt: "Imagem de uma cabeça com tag dentro",
+  nameClass: "-active"
+});
 
 $cardsWrapper.insertAdjacentHTML("beforeend", $CardCabeca);
 $cardsWrapper.insertAdjacentHTML("beforeend", $CardCabeca);
