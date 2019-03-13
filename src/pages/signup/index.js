1b3cc26aa385e38c34fcd47d98619ef5bf09f74b
Cristiano Morgante <cristianomorgante@hotmail.com>
2019-03-13 23:52:49 +0000

    Desafio Avançado, fim da aula [038]

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,7 +1,7 @@
 (function() {
   const $root = document.querySelector("#root");
   const $loginButton = flatButton.render("Log in");
-  const $signupButton = flatButton.render("Sig up");
+  const $signupButton = flatButton.render("Sig up", true);
 
   $root.insertAdjacentHTML("beforeend", $loginButton);
   $root.insertAdjacentHTML("beforeend", $signupButton);
