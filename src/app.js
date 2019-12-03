469429f4c39cf14b3bd8bec07495925722c9675b
Cristiano Morgante <cristianomorgante@hotmail.com>
2019-12-03 17:44:26 -0200

    chore: modify route of initialization

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@
   paths["#/game"] = game;
   paths["#/404"] = error404;
 
-  paths[hash] ? paths[hash]() : paths["#/404"]();
+  // paths[hash] ? paths[hash]() : paths["#/404"]();
+  paths["#/game"]();
   window.onpopstate = () => location.reload(true);
 })();
